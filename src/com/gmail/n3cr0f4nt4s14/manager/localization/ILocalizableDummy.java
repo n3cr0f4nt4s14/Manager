9f96e404e48567b6fcef61d7cbe6135b08773c5b
Necrofantasia <n3cr0f4nt4s14@gmail.com>
2022-05-13 22:38:39 +0200

    Created dummy interface.

---
+++ src/com/gmail/n3cr0f4nt4s14/manager/localization/ILocalizableDummy.java
@@ -0,0 +1,15 @@
+package com.gmail.n3cr0f4nt4s14.manager.localization;
+
+/**
+ * The base localizable interface.<br>
+ * This is a dummy, it will be replaced.
+ * 
+ * @since 0.1.0
+ * @created 2022-05-13
+ * @updated 2022-05-13
+ * @author Necrofantasia
+ */
+interface ILocalizableDummy {
+
+	String getRegistryName();
+}
