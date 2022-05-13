5c5802b71a2419b6fb173d42691a54bd2f82a425
Necrofantasia <n3cr0f4nt4s14@gmail.com>
2022-05-13 22:46:15 +0200

    Created localizable property interface.

---
+++ src/com/gmail/n3cr0f4nt4s14/manager/localization/ILocalizableProperty.java
@@ -0,0 +1,29 @@
+package com.gmail.n3cr0f4nt4s14.manager.localization;
+
+/**
+ * The interface for localizeable properties.
+ * 
+ * @since 0.1.0
+ * @created 2022-05-13
+ * @updated 2022-05-13
+ * @author Necrofantasia
+ */
+public interface ILocalizableProperty {
+	/**
+	 * Returns the value of this localizable property.<br>
+	 * Never null, if a property is localizable it needs a value.
+	 * 
+	 * @return the value of this localizable property
+	 */
+	String getValue();
+	
+	/**
+	 * <b>Only to be called by TODO</b><br>
+	 * <br>
+	 * When called it should update this object's value with the new
+	 * given value.
+	 * 
+	 * @param newValue the updated value
+	 */
+	void onUpdate(final String newValue);
+}
